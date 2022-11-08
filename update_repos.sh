79b65fc1fc828213a530d7d75b8fe1058b60f99d
vladimir <vladimi.sedlar@monsoonconsulting.com>
2022-11-08 23:50:52 +0100

    feat(scm): add update_repos script

---
+++ update_repos.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+### Designed to contain a list of jobs to be executed upon each login
+
+update_repo() {
+  git pull
+}
+
+update_repos() {
+  list_of_dirs=("$HOME/dev/dotfiles" "$HOME/Documents/Books" "$HOME/dev/study" "$HOME/dev/scripts")
+
+  for dir in "${list_of_dirs[@]}"
+  do
+    (cd "$dir" && update_repo)
+  done
+}
+
+update_repos
